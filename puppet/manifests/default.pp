5516e4b46a224368ddf82f0862b1727bec8180d7
adamors <adamors@users.noreply.github.com>
2014-10-12 23:00:18 +0300

    Adding back postgres

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,4 +3,9 @@ class { 'coreutils': vim => false }
 include 'ppa-load'
 include nodejs
 include ruby
+class { 'my-postgresql':
+  db      => 'vagrant',
+  db_user => 'vagrant',
+  db_pass => 'vagrant',
+}
 include ubuntu
